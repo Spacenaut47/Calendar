649c13fd6141356433b3a56ae2da32f11efc87d7
Spacenaut47 <sohammondal81@gmail.com>
2022-04-08 21:58:16 +0530

    Updated script

--- calendar.js
+++ calendar.js
@@ -1,11 +1,11 @@
 const date = new Date()
 date.setDate(1)
-console.log(date.getDay());
 const monthDays = document.querySelector('.days')
 const lastDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate()
+const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate()
 const firstDayIndex = date.getDate()
-
-console.log(lastDay);
+const lastDayIndex = new Date(date.getFullYear(),date.getMonth()+1,0).getDay()
+const nextDays = 7-lastDayIndex
 const month = [
     "January",
     "February",
@@ -26,11 +26,19 @@ document.querySelector('.date p').innerHTML = date.toDateString()
 
 let days = ""
 
+for(let x = firstDayIndex; x > 0;x--)[
+    days += `<div class = "prev-date>${prevLastDay-x+1}</div>`
+]
+
 for(let i = 1;i <= lastDay;i++){
-    days += `<div>${i}</div>`
-    monthDays.innerHTML = days
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class = "today">${i}</div>`
+    }else {
+        days += `<div>${i}</div>`
+    }
 }
 
-for(let x = firstDayIndex; i > 0;x--)[
-    
-]
+for(let j = 1;j < nextDays;j++){
+    days += `<div class = "next-date">${j}</div>`
+    monthDays.innerHTML = days
+}
